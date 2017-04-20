c0e88b2cf02f3ee8193d3eea2bc1cae9410773b1
kyle.nitzsche@canonical.com <kyle.nitzsche@canonical.com>
2017-04-20 22:19:56 +0000

    Have existing user working including sudo

--- bin/run-existing-user-over-ssh.sh
+++ bin/run-existing-user-over-ssh.sh
@@ -1,7 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 USER="knitzsche"
 PASSWORD="ubuntu"
+echo "===== 1"
 echo "===== USER: $USER"
 echo "===== PWRD: $PASSWORD"
+echo -e "#\x21/bin/sh\necho $PASSWORD" >/tmp/askpass.sh
 export PATH_TO_SCRIPT=$SNAP_DATA/my-script.sh
-$SNAP/usr/bin/sshpass -p $PASSWORD ssh $USER@localhost $PATH_TO_SCRIPT 
+ssh -o "PasswordAuthentication=yes" -F $SNAP/config $USER@localhost "$PATH_TO_SCRIPT $PASSWORD"
+#$SNAP/usr/bin/sshpass -p $PASSWORD ssh -F $SNAP/config $USER@localhost $PATH_TO_SCRIPT 
+echo "===== DONE"
--- bin/run-over-ssh.sh
+++ bin/run-over-ssh.sh
@@ -1,13 +1,14 @@
-#!/bin/sh
-USER=”remotemgr”
+#!/bin/bash
+USER=remotemgr
 $SNAP/usr/sbin/useradd --extrausers $USER
 # We need a very secure and random password as nobody should ever able to guess
 # it as its only meant for the internal use.
-PASSWORD=”$(pwgen -y -n -c 32 1)”
+PASSWORD=$(pwgen -y -n -c 32 1)
 echo $USER:$PASSWORD | $SNAP/usr/sbin/chpasswd
 export PATH_TO_SCRIPT=$SNAP_DATA/my-script.sh
-$SNAP/usr/bin/sshpass -p ‘$PASSWORD’ ssh $USER@localhost $PATH_TO_SCRIPT 
-#sshpass -p ‘$PASSWORD’ ssh $USER@localhost $PATH_TO_SCRIPT &> $SNAP_DATA/output.txt 
+#$SNAP/usr/bin/sshpass -p $PASSWORD ssh -F $SNAP/config $USER@localhost $PATH_TO_SCRIPT 
+echo -e "#\x21/bin/sh\necho $PASSWORD" >/tmp/askpass.sh
+sshpass -p "$PASSWORD" ssh -F $SNAP/config $USER@localhost "$PATH_TO_SCRIPT $PASSWORD"
 #/snap/remote/x2/bin/run-over-ssh.sh: 9: /snap/remote/x2/bin/run-over-ssh.sh: cannot create /var/snap/remote/x2/output.txt: Permission denied
 
 $SNAP/usr/sbin/deluser $USER
